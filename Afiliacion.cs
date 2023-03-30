47d0234fb10709c0f5c9223a0759ba5668d09474
Marredxxx <MariaRojas0306@umesoamericana.onmicrosoft.com>
2023-03-30 08:37:17 -0600

    Agregar archivos de proyecto.

---
+++ Afiliacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartidosPoliticps
+{
+    internal class Afiliacion
+    {
+        string partido;
+        int dpi;
+        string fechaAfliacion;
+
+        public string Partido { get => partido; set => partido = value; }
+        public int Dpi { get => dpi; set => dpi = value; }
+        public string FechaAfliacion { get => fechaAfliacion; set => fechaAfliacion = value; }
+    }
+}
